7208940a9863359c70be83cfffe1d90bb6d6b566
yasasvi <yasasvigumma@gmail.com>
2023-11-15 18:48:28 +0530

    creating a table

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // importing SQL library
-const conn = require("sqlite3");
+const sql = require("sqlite3");
+let query;
 
 // Connecting to database
-const db = new conn.Database("./app.db", conn.OPEN_READWRITE, (err) => {
+const db = new sql.Database("./app.db", sql.OPEN_READWRITE, (err) => {
   if (err) return console.error(err.message);
 });
 
 // Creating Table
+query = "create table test(id integer primary key, name, phone_no)";
+db.run(query);
