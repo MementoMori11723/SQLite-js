68a02f0af9fb651f3c2314a7f250d9ab38182650
yasasvi <yasasvigumma@gmail.com>
2023-11-15 19:00:37 +0530

    creating a table

--- app.js
+++ app.js
@@ -8,5 +8,18 @@ const db = new sql.Database("./app.db", sql.OPEN_READWRITE, (err) => {
 });
 
 // Creating Table
-query = "create table test(id integer primary key, name, phone_no)";
-db.run(query);
+// query = "create table test(id integer primary key, name, phone_no)";
+// db.run(query);
+
+// Insert Data
+// query = `insert into test(name,phone_no) values (?,?)`;
+// db.run(query, ["john", "1234567890"], (err) => {
+//   if (err) return console.error(err.message);
+// });
+
+// Showing data
+query = `select * from test`;
+db.all(query, [], (err, rows) => {
+  if (err) return console.error(err.message);
+  rows.forEach((row) => console.log(row));
+});
