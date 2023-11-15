0a5bd729ff2c046dbe9d1b03c2f5bfbccdb402e1
yasasvi <yasasvigumma@gmail.com>
2023-11-15 19:18:44 +0530

    test successful!

---
+++ run.js
@@ -0,0 +1,15 @@
+// importing SQL library
+const sql = require("sqlite3");
+let query;
+
+// Connecting to database
+const db = new sql.Database("./app.db", sql.OPEN_READWRITE, (err) => {
+  if (err) return console.error(err.message);
+});
+
+// Showing data
+query = `select * from test`;
+db.all(query, [], (err, rows) => {
+  if (err) return console.error(err.message);
+  rows.forEach((row) => console.log(row));
+});
